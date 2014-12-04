bbb78d7f14bcee64d36d312e1a15e47dbb46bf3a
Kristine <krissyfivee@gmail.com>
2014-12-04 15:16:42 -0500

    Adding multiple Badges
    Community Service, Academic Award, Dean's List badges.

--- app/src/main/java/com/example/kristine/badgeapp/AvailableBadges.java
+++ app/src/main/java/com/example/kristine/badgeapp/AvailableBadges.java
@@ -12,7 +12,7 @@ public class AvailableBadges extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_your_badges);
+        setContentView(R.layout.activity_available_badges);
         Intent intent = getIntent();
     }
 
